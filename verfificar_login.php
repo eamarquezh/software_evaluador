54aaabfe2dea69c6822cce45470cc1bd3c36d190
noelopmed30 <107820607+noelopmed30@users.noreply.github.com>
2022-08-19 17:55:03 -0500

    Create verfificar_login.php

---
+++ verfificar_login.php
@@ -0,0 +1,24 @@
+<?php
+
+//OBTENER VARIABLES DE LA FUNCION JAVA SCRIPT
+$email     =$_POST["email"];
+$password  =$_POST["password"];
+
+$idusuario=0;
+$consulta="select idcliente from clientes where email='$mail' and password='$password'";
+$recordset = mysqli_query($link,$consulta);
+while($registro = mysqli_fetch_array($recordset)){$idusuario=$registro["idusuario"];}
+
+if(!isset($idusuario)){$idusuario=0;}
+
+if($idusuario==0){ 
+	$_SESSION['ss_idusuario']=0;
+	echo 0; // no existe registro
+}else{
+	$_SESSION['ss_idusuario']=$idcliente;
+	echo 1; // se encontro registro
+}
+
+
+
+?>
